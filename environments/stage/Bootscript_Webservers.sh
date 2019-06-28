b94aa660773ca84122cedb1b5f5bea042fb8a4a0
Robinson Rodriguez <flacorobin@gmail.com>
2019-06-28 13:56:54 -0400

    First Commit - Modules, main project

--- environments/stage/Bootscript_Webservers.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-#Bootscript for web WebServers
-
-sudo yum update -y
-yum install httpd -y
-cd /var/www/html
-echo "<html><h1>This is a WebServer</h1></html>" > index.html
-service httpd start
-chkconfig httpd on
--- environments/stage/modules/webserver-sg/vars.tf
+++
@@ -1,7 +0,0 @@
-variable "webserver_sg_vpc_id" {
-}
-
-variable "webserver_sg_ssh_ip"{
-  type = list
-  default = ["0.0.0.0/0"]
-}
